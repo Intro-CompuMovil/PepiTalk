595b01888ea202cff8b8795183fcd7c7b771ecca
Gabriel13 <gabrielesp7724@gmail.com>
2024-10-14 18:08:25 -0500

    Add: Clases para el manejo de datos.

--- PepiTalk/app/src/main/java/com/example/pepitalk/Datos/Data.kt
+++ PepiTalk/app/src/main/java/com/example/pepitalk/Datos/Data.kt
@@ -3,6 +3,16 @@ package com.example.pepitalk.Datos
 class Data {
     companion object{
         val MY_PERMISSION_REQUEST_CAMERA=0;
-        val MY_PERMISSION_REQUEST_GALLERY=0;
+        val MY_PERMISSION_REQUEST_GALLERY=1;
+        val listaGrupos = mutableListOf<DataGrupo>()
+        val personas = mutableListOf<Persona>()
+        val listaOfertas = mutableListOf<DataOferta>()
+        val listaReuniones = mutableListOf<DataReunion>()
+        var personaLog = Persona(
+            tipo = "Traductor",
+            usuario = "Pepito123",
+            nombre = "Pepito el Pinguino",
+            contrasena = "Pinguino",
+            correo = "pepito@gmail.com")
     }
 }
